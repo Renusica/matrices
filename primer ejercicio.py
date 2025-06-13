4305860894ad108bbdd9ff1a68518800eb51d765
Halam Hidalgo <halamjhidalgotorres@gmail.com>
2025-06-13 15:48:23 -0400

    For fincional

--- primer ejercicio.py
+++ primer ejercicio.py
@@ -1,3 +1,14 @@
 
 
-print("")
+m=[
+["A", "B", "C"],
+["D", "E", "F"],
+["G", "H", "I"]
+]
+
+
+
+for f in m:
+    for c in f:
+        if c == "C":
+            print("Esta en la matriz la letra: ", c)
